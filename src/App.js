332d899697f04ae5ecce48f44efee76bbab3d1d4
Oscar Pacheco <Oscarpa9188@gmail.com>
2022-03-14 01:58:28 -0500

    TailwindCss added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import Navbar from "./Views/Navbar/Navbar";
+import LandingPage from "./Views/Landing_Page/LandingPage";
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>ReactJs Weather application</h1>
+        <Navbar />
       </header>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+      <LandingPage/> 
     </div>
   );
 }
